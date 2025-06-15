3adf069c9fe8921d4d59c705f746db2cddb60897
RealElvince <obuongely@yahoo.com>
2025-06-15 14:46:01 +0300

    updated data_project

---
+++ data_project/airflow/dags/elt_dag.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import sys
+
+
+sys.path.append('/opt/airflow/data_project/scripts')
+
+
+from generate_data import generate_patient_data, generate_doctor_data
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 10, 1),
+    "retries": 1,
+    
+}
+
+with DAG(
+    dag_id="weather_api_orchestrator",
+    description="Orchestrator for Weather API DAGs",
+    tags=["weather_api", "orchestrator"],
+    schedule=timedelta(minutes=5),
+    catchup=False,
+    default_args=default_args,
+) as dag:
+
+    generate_patient_data_task = PythonOperator(
+        task_id="generate_patient_data",
+        python_callable=generate_patient_data,
+        op_kwargs={"num_records": 400},
+    )
+
+    generate_doctor_data_task = PythonOperator(
+        task_id="generate_doctor_data",
+        python_callable=generate_doctor_data,
+        op_kwargs={"num_records": 100},
+    )
+
+    generate_patient_data_task >> generate_doctor_data_task
